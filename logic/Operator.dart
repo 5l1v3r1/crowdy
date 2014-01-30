33c64811d6ac2b9316160cb70a4d1be12a87f68f
Mert Emin Kalender <merteminkalender@gmail.com>
2014-01-29 22:19:48 -0800

    Operator details map added

--- logic/Operator.dart
+++ logic/Operator.dart
@@ -11,9 +11,10 @@ class Operator {
   Map details;
 
   Operator(String this.id, String this.type, num mouseX, num mouseY) {
-    next = new Map<String, bool>();
-    prev = new Map<String, bool>();
-    details = new Map();
+    this.next = new Map<String, bool>();
+    this.prev = new Map<String, bool>();
+    this.details = new Map();
+    this.details['rules'] = new List<String>();
   }
 
   void initialize() {
@@ -92,6 +93,14 @@ class Operator {
     else if (input is html.DivElement) {
       this.details[identifier] = input.text;
     }
+  }
+
+  void cleanRules() {
+    this.details['rules'].clear();
+  }
+
+  void addRule(String value) {
+    (this.details['rules'] as List<String>).add(value);
   }
 
   bool updateDownFlow(String prevId) {
